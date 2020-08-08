b242868bef101b5fde7fdd8d513c89fc47135bc4
Teruaki Oe <teru.o.77@gmail.com>
2020-08-08 07:52:51 -0400

    完成版

--- js/script.js
+++ js/script.js
@@ -28,3 +28,14 @@ $(function() {
     $(window).trigger('scroll');
 
 });
+
+$(function() {
+    $('a[href^="#"]').click(function() {
+        var speed = 300;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({ scrollTop: position }, speed, "swing");
+        return false;
+    });
+});
